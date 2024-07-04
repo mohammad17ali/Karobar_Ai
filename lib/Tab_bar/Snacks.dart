daf74e8b13a0338d1eb15cb00f3e859c93e65020
triCKY05 <6465913@github>
2024-07-04 12:52:03 +0530

    'update'

--- lib/Tab_bar/Snacks.dart
+++ lib/Tab_bar/Snacks.dart
@@ -1,59 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:karobar/Tab_bar/edit.dart';
+
+// import 'package:practice/edit.dart';
 
 class snacks extends StatefulWidget {
   const snacks({Key? key}) : super(key: key);
 
   @override
-  State<snacks> createState() => _InventoryState();
+  State<snacks> createState() => _snacksState();
 }
 
-class _InventoryState extends State<snacks> {
+class _snacksState extends State<snacks> {
+  List<snacksItem> allRecords = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(
+      Uri.parse(
+          'https://api.airtable.com/v0/appgAln53ifPLiXNu/tblvBQeCreDaryIPs'),
+      headers: {
+        'Authorization':
+            'Bearer patXmwDbTcQr2K1lJ.de9224db382239bd6b93f162a21d6b0db884233ee5f59ab1458e5851b6764451',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List<snacksItem> fetchedItems = (data['records'] as List)
+          .map((record) => snacksItem.fromJson(record))
+          .toList();
+
+      setState(() {
+        allRecords = fetchedItems;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  void updateItem(snacksItem updatedItem) {
+    setState(() {
+      int index = allRecords.indexWhere((item) => item.id == updatedItem.id);
+      if (index != -1) {
+        allRecords[index] = updatedItem;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.all(16.0),
-        children: [
-          _buildInventoryCard(
-            itemName: 'samoza',
-            imageUrl:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjvYe10xwM-YcKB5jf_T0pPYJDFhBuhQCw3A&s',
-            price: 10,
-            quantity: 50,
-            remaining: 100,
-            popularity: 4.5,
-          ),
-          _buildInventoryCard(
-            itemName: 'Burger',
-            imageUrl:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrPrrLTujmQNKD3b4xIrfkqAhagt9qei_qqg&s',
-            price: 20,
-            quantity: 3,
-            remaining: 7,
-            popularity: 4.2,
-          ),
-          _buildInventoryCard(
-            itemName: 'Pizza',
-            imageUrl:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIibPbOeDQQscm9g-fDNdCvROokQJukg8nYQ&s',
-            price: 60,
-            quantity: 3,
-            remaining: 7,
-            popularity: 4.2,
-          ),
-          // Add more cards as needed
-        ],
-      ),
+      body: allRecords.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              padding: EdgeInsets.all(16.0),
+              itemCount: allRecords.length,
+              itemBuilder: (context, index) {
+                final item = allRecords[index];
+                return _buildsnacksCard(
+                  item: item,
+                  onUpdate: updateItem,
+                );
+              },
+            ),
     );
   }
 
-  Widget _buildInventoryCard({
-    required String itemName,
-    required String imageUrl,
-    required int price,
-    required int quantity,
-    required int remaining,
-    required double popularity,
+  Widget _buildsnacksCard({
+    required snacksItem item,
+    required Function(snacksItem) onUpdate,
   }) {
     double screenWidth = MediaQuery.of(context).size.width;
     bool isSmallScreen = screenWidth < 600;
@@ -75,17 +98,15 @@ class _InventoryState extends State<snacks> {
               ),
               child: Center(
                 child: Text(
-                  itemName,
+                  item.itemName,
                   style: TextStyle(
-                    fontSize: isSmallScreen ? 20 : 24,
+                    fontSize: isSmallScreen ? 16 : 18,
                     fontWeight: FontWeight.bold,
-
                     color: Colors.white, // Text color
                   ),
                 ),
               ),
             ),
-            // Divider(),
             Container(
               color: Color(0xFFA3D9FF),
               child: Row(
@@ -93,7 +114,7 @@ class _InventoryState extends State<snacks> {
                   Expanded(
                     flex: isSmallScreen ? 3 : 3,
                     child: Image.network(
-                      imageUrl,
+                      item.pictureUrl,
                       height: 100,
                       width: 300,
                     ),
@@ -104,10 +125,14 @@ class _InventoryState extends State<snacks> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        _buildInfoRow('Price', '$price'),
-                        _buildInfoRow('Quantity', quantity.toString()),
-                        _buildInfoRow('Remaining', remaining.toString()),
-                        _buildInfoRow('Popularity', popularity.toString()),
+                        _buildInfoRow('Price', '${item.price}'),
+                        _buildInfoRow('Quantity', item.quantity.toString()),
+                        _buildInfoRow(
+                            'Remaining',
+                            item.quantity
+                                .toString()), // Assuming remaining is the same as quantity
+                        _buildInfoRow('Popularity',
+                            '4.5'), // Replace with actual popularity if available
                       ],
                     ),
                   ),
@@ -119,15 +144,24 @@ class _InventoryState extends State<snacks> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  'Rs. $price',
+                  'Rs. ${item.price}',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: isSmallScreen ? 14 : 16,
                   ),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    // Implement edit functionality
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            EditsnacksItem(item: item, onUpdate: onUpdate),
+                      ),
+                    );
+                    if (result != null && result is snacksItem) {
+                      onUpdate(result);
+                    }
                   },
                   style: ButtonStyle(
                     backgroundColor:
@@ -165,4 +199,36 @@ void main() {
   runApp(MaterialApp(
     home: snacks(),
   ));
+}
+
+class snacksItem {
+  final String id;
+  final String pictureUrl;
+  final String itemName;
+  final int price;
+  final int quantity;
+  final String category;
+  final String itemID;
+
+  snacksItem({
+    required this.id,
+    required this.pictureUrl,
+    required this.itemName,
+    required this.price,
+    required this.quantity,
+    required this.category,
+    required this.itemID,
+  });
+
+  factory snacksItem.fromJson(Map<String, dynamic> json) {
+    return snacksItem(
+      id: json['id'],
+      pictureUrl: json['fields']['Picture'][0]['url'],
+      itemName: json['fields']['Item Name'],
+      price: json['fields']['Price'],
+      quantity: json['fields']['Quantity'],
+      category: json['fields']['Category'],
+      itemID: json['fields']['ItemID'],
+    );
+  }
 }
---
+++ lib/Tab_bar/edit.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:karobar/Tab_bar/Snacks.dart';
+
+class EditsnacksItem extends StatefulWidget {
+  final snacksItem item;
+  final Function onUpdate;
+
+  const EditsnacksItem({Key? key, required this.item, required this.onUpdate})
+      : super(key: key);
+
+  @override
+  State<EditsnacksItem> createState() => _EditsnacksItemState();
+}
+
+class _EditsnacksItemState extends State<EditsnacksItem> {
+  late TextEditingController itemNameController;
+  late TextEditingController priceController;
+  late TextEditingController quantityController;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    itemNameController = TextEditingController(text: widget.item.itemName);
+    priceController = TextEditingController(text: widget.item.price.toString());
+    quantityController =
+        TextEditingController(text: widget.item.quantity.toString());
+  }
+
+  Future<void> _updateItem() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final response = await http.patch(
+      Uri.parse(
+          'https://api.airtable.com/v0/appgAln53ifPLiXNu/tblvBQeCreDaryIPs/${widget.item.id}'),
+      headers: {
+        'Authorization':
+            'Bearer patXmwDbTcQr2K1lJ.de9224db382239bd6b93f162a21d6b0db884233ee5f59ab1458e5851b6764451',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'fields': {
+          'Item Name': itemNameController.text,
+          'Price': int.parse(priceController.text),
+          'Quantity': int.parse(quantityController.text),
+        }
+      }),
+    );
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (response.statusCode == 200) {
+      final updatedItem = snacksItem.fromJson(json.decode(response.body));
+      widget.onUpdate(updatedItem);
+      Navigator.pop(context);
+    } else {
+      throw Exception('Failed to update item');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Item'),
+        backgroundColor: Color(0xFF195DAD),
+        titleTextStyle: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            color: Color.fromARGB(255, 101, 178, 233),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  color: Color(0xFFA3D9FF),
+                  margin: EdgeInsets.all(100),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: itemNameController,
+                          decoration: InputDecoration(
+                            labelText: 'Item Name',
+                            labelStyle: TextStyle(
+                              fontSize: 30,
+                              color: Colors.black, // Increased label text size
+                            ),
+                          ),
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF195DAD),
+                            fontWeight:
+                                FontWeight.bold, // Increased input text size
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        TextField(
+                          controller: priceController,
+                          decoration: InputDecoration(
+                            labelText: 'Price',
+                            labelStyle: TextStyle(
+                              fontSize: 30,
+                              color: Colors.black, // Increased label text size
+                            ),
+                          ),
+                          keyboardType: TextInputType.number,
+                          style: TextStyle(
+                            fontSize: 20, // Increased input text size
+                            color: Color(0xFF195DAD),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        TextField(
+                          controller: quantityController,
+                          decoration: InputDecoration(
+                            labelText: 'Quantity',
+                            labelStyle: TextStyle(
+                              fontSize: 30,
+                              color: Colors.black, // Increased label text size
+                            ),
+                          ),
+                          keyboardType: TextInputType.number,
+                          style: TextStyle(
+                            fontSize: 20, // Increased input text size
+                            color: Color(0xFF195DAD),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 60),
+                        ElevatedButton(
+                          onPressed: _updateItem,
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Color(0xFF195DAD)),
+                            foregroundColor: MaterialStateProperty.all<Color>(
+                                Colors.white), // Text color
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                              EdgeInsets.symmetric(
+                                  vertical: 20.0,
+                                  horizontal: 50.0), // Adjust padding as needed
+                            ),
+                          ),
+                          child: Text(
+                            'Update',
+                            style: TextStyle(
+                                fontSize:
+                                    20), // Increase text size if necessary
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  strokeWidth: 10,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
