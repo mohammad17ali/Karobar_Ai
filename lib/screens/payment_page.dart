c1f08b4b619c525e6d181f39e41f8644a5de38aa
Mohammad Ali <gobalee10@gmail.com>
2024-11-25 00:51:54 +0530

    Create payment_page.dart

---
+++ lib/screens/payment_page.dart
@@ -0,0 +1,12 @@
+// lib/screens/PaymentPage.dart
+import 'package:flutter/material.dart';
+
+class PaymentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('PaymentPage')),
+      body: Center(child: Text('Welcome to Karobar')),
+    );
+  }
+}
