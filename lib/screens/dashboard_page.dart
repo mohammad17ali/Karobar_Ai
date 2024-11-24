315b75d8af847d16cd14d6423fbdf0e5a52f7a35
Mohammad Ali <gobalee10@gmail.com>
2024-11-25 00:51:22 +0530

    Create dashboard_page.dart

---
+++ lib/screens/dashboard_page.dart
@@ -0,0 +1,12 @@
+// lib/screens/DashboardPage.dart
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('DashboardPage')),
+      body: Center(child: Text('Welcome to Karobar')),
+    );
+  }
+}
