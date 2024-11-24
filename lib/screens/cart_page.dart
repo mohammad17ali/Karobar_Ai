118e5497e562650b283141712c7e5087aca458c0
Mohammad Ali <gobalee10@gmail.com>
2024-11-25 00:50:52 +0530

    Create cart_page.dart

---
+++ lib/screens/cart_page.dart
@@ -0,0 +1,12 @@
+// lib/screens/cart_page.dart
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('CartPage')),
+      body: Center(child: Text('Welcome to Karobar')),
+    );
+  }
+}
