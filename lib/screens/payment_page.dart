b682856271ff01e89568c1acf72d37be05028627
Mohammad Ali <gobalee10@gmail.com>
2024-11-26 01:58:17 +0530

    Update payment_page.dart

--- lib/screens/payment_page.dart
+++ lib/screens/payment_page.dart
@@ -5,8 +5,10 @@ class PaymentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('PaymentPage')),
-      body: Center(child: Text('Welcome to Karobar')),
+      appBar: TopBar(), // Add the TopBar here
+      body: Center(
+        child: Text('PaymentPage!'),
+      ),
     );
   }
 }
