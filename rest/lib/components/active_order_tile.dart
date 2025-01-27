abdf0547c1f4a76960afc87c1acb664cbc6bb475
Mohammad Ali <gobalee10@gmail.com>
2025-01-28 03:21:32 +0530

    Update active_order_tile.dart

--- rest/lib/components/active_order_tile.dart
+++ rest/lib/components/active_order_tile.dart
@@ -1,9 +1,11 @@
+// active_order_tile.dart
 import 'package:flutter/material.dart';
 import '../constants/constants.dart';
+import 'order_details_card.dart';
 
 class ActiveOrderTile extends StatelessWidget {
   final Map<String, dynamic> order;
-  
+
   const ActiveOrderTile({
     super.key,
     required this.order,
@@ -11,34 +13,43 @@ class ActiveOrderTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: AppDecorations.gridTileDecoration(context),
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Order No.",
-            style: const TextStyle(
-              fontSize: 8,
-              color: AppColors.accent,
-              //fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => OrderDetailsCard(order: order),
+          ),
+        );
+      },
+      child: Container(
+        decoration: AppDecorations.gridTileDecoration(context),
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Order No.",
+              style: TextStyle(
+                fontSize: 8,
+                color: AppColors.accent,
+              ),
             ),
-          ),
-          Text(
-            "${order['OrderNum']}",
-            style: const TextStyle(
-              fontSize: 24,
-              color: AppColors.primary,
-              fontWeight: FontWeight.bold,
+            Text(
+              "${order['OrderNum']}",
+              style: const TextStyle(
+                fontSize: 24,
+                color: AppColors.primary,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            "${order['Amount']} Rs.",
-            style: AppTextStyles.priceText,
-          ),
-        ],
+            const SizedBox(height: 4),
+            Text(
+              "${order['Amount']} Rs.",
+              style: AppTextStyles.priceText,
+            ),
+          ],
+        ),
       ),
     );
   }
